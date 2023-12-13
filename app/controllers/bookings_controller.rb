7430bb22a83c8b1d57918d58502f27fe6cdac0d7
Cvetomir Kecman <cvejka.kecman@gmail.com>
2023-12-13 16:10:26 +0100

    Project finished

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,30 @@
 class BookingsController < ApplicationController
+  include BookingsHelper
+
   def new
     @booking = Booking.new
+    params[:passenger_count].to_i.times { @booking.passengers.build }
     @flight = Flight.find(params[:flight].to_i)
   end
   
   def create
+    @booking = Booking.new(booking_params)
+    @booking.flight_id = params[:booking][:flight_id].to_i
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      @flight = Flight.find(params[:booking][:flight_id])
+      render :new
+    end
+  end
 
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
   end
 end
--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,14 @@
 module BookingsHelper
+  def ordinal_number(index)
+    case index
+    when 0
+      'First'
+    when 1
+      'Second'
+    when 2
+      'Third'
+    else
+      "#{index + 1}th"
+    end
+  end
 end
